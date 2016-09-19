7de0c423d01824e560ba3c8c6ddf79bf98f805ea
xxdamage <xxxdamagee@gmail.com>
2016-09-19 23:39:28 +0200

    Update anti-spampro.lua
    añadido boton inline para desbanear

--- plugins/anti-spampro.lua
+++ plugins/anti-spampro.lua
@@ -1,6 +1,8 @@
 -- detecta alias de canales y supergrupos
 
 local action = function(msg, blocks)
+local id = msg.from.id
+local name = msg.from.first_name
 	if msg.chat.type == 'private' or roles.is_admin(msg) then return true end
 	if not msg.entities then return true end
 
@@ -18,7 +20,7 @@ local action = function(msg, blocks)
 			chat = api.getChat(canal)
 			if chat then
 				if chat.ok == true then
-					api.sendReply(msg, msg.from.first_name..' ('..msg.from.id..') *ha sido baneado por hacer spam*\n\n` 🔹Informe enviado a los administradores`', true)
+					api.sendKeyboard(msg.chat.id, name.. ' ('..id.. ') ha sido *banead@* por hacer SPAM 🔨\n\n🔸 `Informe enviado al administrador`', {inline_keyboard = {{{text = 'Desbanear', callback_data = 'unban:'..id}}}}, true))
 					api.banUser(msg.chat.id, msg.from.id)
 					misc.forwardToAdmins(msg.chat.id, msg.message_id)
 					misc.sendMessageToAdmins(msg.chat.id, '👆 SPAM en el grupo: ➡️ *'..msg.chat.title..'*')
